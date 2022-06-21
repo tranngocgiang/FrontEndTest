0716b612f388719f9a74876633d5fa37daed3428
tranngocgiang <tranngocgiang0311@gmail.com>
2022-06-21 23:44:10 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function handleClick() {
+    var slidebar_mb = document.getElementById('slidebar_mb');
+    var slidebar_id = document.getElementById('slidebar_id');
+    slidebar_mb.classList.toggle('active')
+    slidebar_id.classList.toggle('active')
+}
